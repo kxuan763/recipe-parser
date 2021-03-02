656f319c474cbdbeeba1352e365da0f81d761fcb
kxuan763 <kennethmxuan@gmail.com>
2021-03-01 22:44:40 -0600

    reimplemented ingredient extraction

--- parse.py
+++ parse.py
@@ -5,10 +5,14 @@ from bs4 import BeautifulSoup
 import nltk
 
 URL = 'https://www.allrecipes.com/recipe/17167/sicilian-spaghetti/'
+URL = 'https://www.allrecipes.com/recipe/279987/'
 
-def extract_text(url):
-    page = requests.get(URL)
+def load_page(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, "lxml")
+    return soup
+
+def extract_text(soup):
     name = soup.find("h1", class_="headline").text
     #print(name)
 
@@ -18,14 +22,14 @@ def extract_text(url):
     for i in ingredients:
         list_i.append(i.text.strip())
         #print(i.text.strip())
-
+    
     #print('directions')
     list_d = []
     directions = soup.find_all("div", class_="paragraph")
     for d in directions:
         list_d.append(d.text)
         #print(d.text)
-    
+
     return(name, list_i, list_d)
 
 # 
@@ -77,18 +81,53 @@ def parse_step(text):
 
 # test_strings = ['Bake the fish in a pan for 30 minutes!', 'Bring 2 cups of water to a boil in a large pot for 4 hours', 'Fry the chicken in the largest pan you have']
 
+fractiondict = {'½':0.5, '⅓':0.333, '⅔': 0.667, '¼':0.25, '¾':0.75, '⅝':0.625, '⅛':0.125, '⅜':0.375, '⅞':0.875, '1 ½':1.5}
+
+list_of_ingredients = ['pineapple', 'bell pepper', 'carrot', 'onion', 'avocado','avocado oil', 'salt', 'pepper', 'chicken', 'water', 'cornstarch', 'brown sugar', 'rice vinegar', 'ketchup', 'soy sauce', 'chile-garlic sauce', 'ginger-garlic paste', 'white pepper']
+measures = ['tablespoon', 'teaspoon', 'pound', 'cup', 'ounce']
+
+# need to integrate into extract_text
+def parse_ingredients(ingredients):
+    list_ingredient = []
+    for i in ingredients:
+        list_ingredient.append(parse_ingredient(i))
+    return list_ingredient
+
 def parse_ingredient(ingredient):
+    # ingredient name
+    length = 0
+    match = ''
+    for i in list_of_ingredients:
+        if i in ingredient:
+            if len(i) > length:
+                match = i
+                length = len(i)
+    # measure
+    measure = ''
+    for m in measures:
+        if m in ingredient:
+            measure = m
+    # quantity
+    quantity = 0
     tokens = nltk.word_tokenize(ingredient)
-    tagged_tokens = nltk.pos_tag(tokens)
-    return tagged_tokens
+    for t in tokens:
+        if t in fractiondict:
+            quantity += fractiondict[t]
+        elif t.isnumeric():
+            quantity += int(t)     
+    return {match: [measure, quantity]}
+
+
+# ingredients structure: {ingredient name: [quantity, measurement, descriptors]}
 
 # for test_string in test_strings:
 #     print(parse_step(test_string))
-recipe = extract_text(URL)
+#recipe = extract_text(URL)
 """ for s in recipe[2]:
     print(s)
     print(parse_step(s)) """
 
+p = load_page(URL)
+recipe = extract_text(p)
 print(recipe[1])
-for i in recipe[1]:
-    print(parse_ingredient(i))
+print(parse_ingredients(recipe[1]))
