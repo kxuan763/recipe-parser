43377cf8688b62b0cf9842cdddb8e40e19ab0988
kxuan763 <kennethmxuan@gmail.com>
2021-03-06 01:37:12 -0600

    implemented main function and input

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # recipe-parser
 Project 2: Recipe Parser &amp; Interactive Cookbook
+
+dependencies:
+pip install requests
+pip install nltk
+pip install bs4
+
+
+run 
+python parse.py
+
+main prompts for recipe url and desired transformation, outputs transformed recipe
--- parse.py
+++ parse.py
@@ -1,8 +1,43 @@
 import requests
-
+import nltk
 from bs4 import BeautifulSoup
 
-import nltk
+# ingredient corpuses
+beans_and_legumes = ['black bean', 'black-eyed pea', 'cannellini bean', 'chickpea', 'fava bean', 'great northern bean', 'kidney bean', 'lentil bean', 'lima bean', 'pinto bean', 'soybean', 'edamame', 'white bean']
+meat_and_poultry = ['beef', 'chicken', 'wild game', 'goat', 'ham', 'lamb', 'pork', 'sausage', 'turkey']
+wild_game = ['venison', 'elk', 'duck', 'goose', 'buffalo', 'bison', 'rabbit']
+chocolate = ['chocolate', 'cocoa']
+oil = ['coconut oil', 'olive oil', 'sesame oil', 'avocado oil', 'vegetable oil', 'canola oil', 'peanut oil', 'oil', 'cooking oil']
+dairy = ['cheese', 'butter', 'buttermilk', 'sour cream', 'egg']
+extracts = ['vanilla extract', 'anise extract', 'peppermint extract', 'lemon extract', 'orange extract', 'butter extract', 'almond extract', 'maple extract', 'rum extract']
+flours = ['white rice flour', 'tapioca flour', 'chickpea flour', 'almond meal', 'coconut flour', 'brown rice flour', 'soy flour', 'corn flour', 'oat flour']
+fruit = ['apple', 'apricot', 'avocado', 'banana', 'berry', 'cranberry', 'blueberry', 'raspberry', 'strawberry', 'blackberry', 'cherry', 'citrus', 'lemon', 'orange', 'lime', 'grapefruit', 'coconut', 'dates', 'fig', 'fruit', 'grape', 'kiwi', 'mango', 'melon', 'watermelon', 'cantaloupe', 'honeydew', 'nectarine', 'papaya', 'peach', 'pear', 'persimmon', 'pineapple', 'plum', 'pomegranate', 'raisin', 'tamarind']
+herbs = ['basil', 'bay leaves', 'bay leaf' 'chervil', 'chives', 'cilantro', 'dill', 'lemongrass', 'marjoram', 'dill', 'lemongrass', 'marjoram', 'mint', 'oregano', 'parsley', 'rosemary', 'sage', 'savory', 'tarragon', 'thyme', 'lavender', 'rose']
+spices = ['seasoning', 'allspice', 'anise', 'annatto', 'cajun', 'caraway', 'cardamom', 'celery seed', 'chili powder', 'cinnamon', 'cloves', 'coriander', 'cumin', 'curry', 'curry powder', 'fennel seed', 'fenugreek', 'garam masala', 'jerk spice', 'mace', 'mustard', 'nutmeg', 'paprika', 'pickling spice', 'poppy seed', 'cayenne pepper', 'saffron', 'sesame seed', 'turmeric', 'vanilla bean', 'white pepper', 'peppercorn', 'ginger', 'star anise']
+mushrooms = ['chanterelle mushroom', 'crimini mushroom', 'enoki mushroom', 'morel mushroom', 'oyster mushroom', 'porcini mushroom', 'portobello mushroom', 'shiitake mushroom']
+nuts_and_seeds = ['chia seed', 'peanut', 'peanut butter', 'pecan', 'almond', 'flax seed', 'walnut', 'amaranth']
+shellfish = ['clam', 'crab', 'crawfish', 'lobster', 'mussel', 'octopus', 'squid', 'oyster', 'scallop', 'shrimp']
+vegetables = ['vegetable', 'artichoke', 'artichoke', 'asparagus', 'beet', 'bell pepper', 'bok choy', 'broccoli', 'brussels sprout', 'mushroom', 'green bean', 'corn', 'cucumber', 'eggplant', 'fennel', 'garlic', 'greens', 'green pea', 'pea', 'radish', 'rhubarb', 'sweet potato', 'tomato', 'tomatillo', 'nopales', 'turnip', 'snow pea', 'sugar snap pea', 'potato', 'squash', 'carrot', 'mixed vegetable', 'cauliflower', 'cabbage', 'leek', 'onion', 'parsnip', 'rutabaga', 'shallot', 'yam', 'water chestnut', 'jicama', 'okra', 'chile pepper', 'olive', 'celery root', 'celery']
+grain = ['barley', 'rice', 'buckwheat', 'bulgur', 'cornmeal', 'millet', 'oat', 'quinoa', 'spelt']
+carbs = grain + ['noodles', 'bread', 'tortillas', 'noodle', 'tortilla', 'pasta']
+misc = ['salt', 'black pepper', 'water', 'sugar', 'vinegar', 'ketchup', 'mustard', 'soy sauce', 'paste']
+fish = ['salmon', 'cod', 'herring', 'mahi-mahi', 'mackerel', 'perch', 'rainbow trout', 'trout', 'sardines', 'bass',
+		'striped bass', 'tuna', 'shark', 'swordfish', 'grouper', 'haddock', 'halibut', 'mahi', 'albacore', 'carp',
+		'monkfish', 'snapper', 'sole', 'trout', 'rockfish', 'mullet', 'whitefish', 'saltfish', 'marlin', 'kingfish',
+		'torsk', 'bonito']
+sauces = ['sauce', 'alfredo', 'alfredo sauce', 'chutney', 'mayonnaise', 'soy sauce', 'barbecue sauce',
+		'mushroom sauce', 'hot sauce', 'peanut sauce', 'hollandaise sauce', 'tomato sauce',
+		'pesto', 'agrodolce sauce', 'agrodolce', 'tkemali', 'tkemali sauce', 'tartar', 'tartar sauce',
+		'marie rose', 'marie rose sauce', 'demi-glace', 'demi-glace sauce', 'salsa', 'gravy', 'steak sauce',
+		'cocktail sauce', 'red sauce', 'white sauce', 'chimichurri', 'marinara', 'marinara sauce',
+		'worcestershire sauce', 'worcestershire', 'ketchup', 'mustard', 'hummus', 'tzatziki', 'ragu',
+		'teriyaki', 'tahini', 'aioli', 'checca', 'checca sauce', 'amatriciana', 'amatriciana sauce', 'guacamole',
+		'fish sauce', 'duck sauce', 'sweet and sour sauce']
+
+MEATS = meat_and_poultry + wild_game + fish
+
+list_of_ingredients = beans_and_legumes + meat_and_poultry + wild_game + chocolate + oil + dairy + extracts + flours + fruit + herbs + spices + mushrooms + nuts_and_seeds + shellfish + vegetables + grain + misc + sauces + fish
+
 
 URL = 'https://www.allrecipes.com/recipe/17167/sicilian-spaghetti/'
 URL = 'https://www.allrecipes.com/recipe/279987/'
@@ -42,15 +77,12 @@ ALL_METHODS = ['refrigerate', 'toss', 'sprinkle', 'whisk', 'press', 'bake', 'sau
 # Times
 TIME_WORDS = ['minute', 'minutes', 'second', 'seconds', 'hour', 'hours']
 # For vegetarian transformations
-MEATS = ['chicken', 'duck', 'venison', 'rabbit', 'bison', 'goat', 'shrimp', 'clams', 'lobster', 'crab', 'beef', 'steak', 'pork', 'ham', 'turkey', 'fish', 'tuna', 'salmon', 'cod', 'lamb']
+#MEATS = ['chicken', 'duck', 'venison', 'rabbit', 'bison', 'goat', 'shrimp', 'clams', 'lobster', 'crab', 'beef', 'steak', 'pork', 'ham', 'turkey', 'fish', 'tuna', 'salmon', 'cod', 'lamb']
 VEG_PROTEINS = ['tofu', 'beans', 'lentils', 'chickpeas']
 # For healthy transformations
 UNHEALTHY_ING = ['butter', 'sugar', 'salt', 'oil', 'cheese', 'dressing', 'ketchup', 'mayonnaise']
 # Because fractions suck and so does allrecipes.com
 fractiondict = {'½':0.5, '⅓':0.333, '⅔': 0.667, '¼':0.25, '¾':0.75, '⅝':0.625, '⅛':0.125, '⅜':0.375, '⅞':0.875, '1 ½':1.5}
-
-# regex all search conditions
-
 
 # parse_steps calls parse_step on all text in list of steps and returns a dictionary of all steps
 def parse_steps(steps, ingredients):
@@ -61,10 +93,6 @@ def parse_steps(steps, ingredients):
     for i in range(len(steps)):
         stepdict[i+1] = parse_step(steps[i], ingredients)
     return stepdict
-
-
-
-
 
 # parse_step(str:text, listof str:ingredients) -> tools:listof str, methods:listof str, times: listof str
 def parse_step(text, ingredients):
@@ -147,8 +175,9 @@ def make_unhealthy(ingredients):
 
 # Mutator that multiplies quantites in ingredients by scale factor
 def scale_ingredient(ingredients, scale_factor):
-    for ingredient in ingredient.keys():
-        ingredients[ingredient][0] *= scale_factor
+    for ingredient in ingredients.keys():
+        if (ingredients[ingredient][0]):
+            ingredients[ingredient][0] *= scale_factor 
 
 def make_italian(ingredients, parsed_recipe):
     for step_no in parsed_recipe.keys():
@@ -158,7 +187,7 @@ def make_italian(ingredients, parsed_recipe):
                 ingr[j] = 'pasta'
     keys_to_remove = []
     for key in ingredients.keys():
-        if any(carb in ingr[j] for carb in carbs):
+        if any(carb in key for carb in carbs):
             keys_to_remove.append(key)
     for key in keys_to_remove:
         value = ingredients.pop(key)
@@ -172,49 +201,17 @@ def make_chinese(ingredients, parsed_recipe):
                 ingr[j] = 'rice'
     keys_to_remove = []
     for key in ingredients.keys():
-        if any(carb in ingr[j] for carb in carbs):
+        if any(carb in key for carb in carbs):
             keys_to_remove.append(key)
     for key in keys_to_remove:
         value = ingredients.pop(key)
         ingredients['rice'] = value
 
-
-
-
-
-
 # test_strings = ['Bake the fish in a pan for 30 minutes!', 'Bring 2 cups of water to a boil in a large pot for 4 hours', 'Fry the chicken in the largest pan you have']
-
 
 fractiondict = {'½':0.5, '⅓':0.333, '⅔': 0.667, '¼':0.25, '¾':0.75, '⅝':0.625, '⅛':0.125, '⅜':0.375, '⅞':0.875, '1 ½':1.5}
 
-#list_of_ingredients = ['pineapple', 'bell pepper', 'carrot', 'onion', 'avocado','avocado oil', 'salt', 'pepper', 'chicken', 'water', 'cornstarch', 'brown sugar', 'rice vinegar', 'ketchup', 'soy sauce', 'chile-garlic sauce', 'ginger-garlic paste', 'white pepper']
 measures = ['tablespoon', 'teaspoon', 'pound', 'cup', 'ounce']
-beans_and_legumes = ['black bean', 'black-eyed pea', 'cannellini bean', 'chickpea', 'fava bean', 'great northern bean', 'kidney bean', 'lentil bean', 'lima bean', 'pinto bean', 'soybean', 'edamame', 'white bean']
-meat_and_poultry = ['beef', 'chicken', 'wild game', 'goat', 'ham', 'lamb', 'pork', 'sausage', 'turkey']
-wild_game = ['venison', 'elk', 'duck', 'goose', 'buffalo', 'bison', 'rabbit']
-chocolate = ['chocolate', 'cocoa']
-oil = ['coconut oil', 'olive oil', 'sesame oil', 'avocado oil', 'vegetable oil', 'canola oil', 'peanut oil']
-dairy = ['cheese', 'butter', 'buttermilk', 'sour cream', 'egg']
-extracts = ['vanilla extract', 'anise extract', 'peppermint extract', 'lemon extract', 'orange extract', 'butter extract', 'almond extract', 'maple extract', 'rum extract']
-flours = ['white rice flour', 'tapioca flour', 'chickpea flour', 'almond meal', 'coconut flour', 'brown rice flour', 'soy flour', 'corn flour', 'oat flour']
-fruit = ['apple', 'apricot', 'avocado', 'banana', 'berry', 'cranberry', 'blueberry', 'raspberry', 'strawberry', 'blackberry', 'cherry', 'citrus', 'lemon', 'orange', 'lime', 'grapefruit', 'coconut', 'dates', 'fig', 'fruit', 'grape', 'kiwi', 'mango', 'melon', 'watermelon', 'cantaloupe', 'honeydew', 'nectarine', 'papaya', 'peach', 'pear', 'persimmon', 'pineapple', 'plum', 'pomegranate', 'raisin', 'tamarind']
-herbs = ['basil', 'bay leaves', 'bay leaf' 'chervil', 'chives', 'cilantro', 'dill', 'lemongrass', 'marjoram', 'dill', 'lemongrass', 'marjoram', 'mint', 'oregano', 'parsley', 'rosemary', 'sage', 'savory', 'tarragon', 'thyme', 'lavender', 'rose']
-spices = ['allspice', 'anise', 'annatto', 'cajun', 'caraway', 'cardamom', 'celery seed', 'chili powder', 'cinnamon', 'cloves', 'coriander', 'cumin', 'curry', 'curry powder', 'fennel seed', 'fenugreek', 'garam masala', 'jerk spice', 'mace', 'mustard', 'nutmeg', 'paprika', 'pickling spice', 'poppy seed', 'cayenne pepper', 'saffron', 'sesame seed', 'turmeric', 'vanilla bean', 'white pepper', 'peppercorn', 'ginger', 'star anise']
-mushrooms = ['chanterelle mushroom', 'crimini mushroom', 'enoki mushroom', 'morel mushroom', 'oyster mushroom', 'porcini mushroom', 'portobello mushroom', 'shiitake mushroom']
-nuts_and_seeds = ['chia seed', 'peanut', 'peanut butter', 'pecan', 'almond', 'flax seed', 'walnut', 'amaranth']
-shellfish = ['clam', 'crab', 'crawfish', 'lobster', 'mussel', 'octopus', 'squid', 'oyster', 'scallop', 'shrimp']
-vegetables = ['artichoke', 'artichoke', 'asparagus', 'beet', 'bell pepper', 'bok choy', 'broccoli', 'brussels sprout', 'mushroom', 'green bean', 'corn', 'cucumber', 'eggplant', 'fennel', 'garlic', 'greens', 'green pea', 'pea', 'radish', 'rhubarb', 'sweet potato', 'tomato', 'tomatillo', 'nopales', 'turnip', 'snow pea', 'sugar snap pea', 'potato', 'squash', 'carrot', 'mixed vegetable', 'cauliflower', 'cabbage', 'leek', 'onion', 'parsnip', 'rutabaga', 'shallot', 'yam', 'water chestnut', 'jicama', 'okra', 'chile pepper', 'olive', 'celery root', 'celery']
-grain = ['barley', 'rice', 'buckwheat', 'bulgur', 'cornmeal', 'millet', 'oat', 'quinoa', 'spelt']
-carbs = grain + ['noodles', 'bread', 'tortillas', 'noodle', 'tortilla', 'pasta']
-misc = ['salt', 'black pepper', 'water', 'sugar', 'vinegar', ]
-sauces = ['ketchup', 'mustard', 'soy sauce']
-
-#todo: add sauces and other missing kitchen staples
-#todo: find preparation/descriptor words
-
-list_of_ingredients = beans_and_legumes + meat_and_poultry + wild_game + chocolate + oil + dairy + extracts + flours + fruit + herbs + spices + mushrooms + nuts_and_seeds + shellfish + vegetables + grain + misc
-
 
 # need to integrate into extract_text
 def parse_ingredients(ingredients):
@@ -257,31 +254,13 @@ def parse_recipe(recipe):
     steps = parse_steps(recipe[2], ingredients.keys())
     return ingredients, steps
 
-
-
 # ingredients structure: {ingredient name: [quantity, measurement, descriptors]}
-    #tagged_tokens = nltk.pos_tag(tokens)
-    #return tokens
 
-# TESTING
-# for test_string in test_strings:
-#     print(parse_step(test_string))
-
-#recipe = extract_text(URL)
-""" for s in recipe[2]:
-    print(s)
-    print(parse_step(s)) """
-
-p = load_page(URL)
-recipe = extract_text(p)
-ingredients, steps = parse_recipe(recipe)
-
-
-print(ingredients, steps)
-make_veg(ingredients, steps)
-print(ingredients, steps)
-make_unveg(ingredients, steps)
-print(ingredients, steps)
+#print(ingredients, steps)
+#make_veg(ingredients, steps)
+#print(ingredients, steps)
+#make_unveg(ingredients, steps)
+#print(ingredients, steps)
 
 def make_human_readable_ingredients(ingredients):
     print('INGREDIENTS:')
@@ -290,8 +269,6 @@ def make_human_readable_ingredients(ingredients):
             if x!='':
                 print(x, end=" ")
         print(i)
-
-make_human_readable_ingredients(ingredients)
 
 def make_human_readable_steps(steps):
     print('STEPS:')
@@ -304,30 +281,44 @@ def make_human_readable_steps(steps):
             print('\n')
         print('\n')
 
-make_human_readable_steps(steps)
+def human_format(ingredients, steps):
+    make_human_readable_ingredients(ingredients)
+    make_human_readable_steps(steps)
 
-# print(*range(5), sep=", ")
-
-# VEGETARIAN SUBSTITUTION LOGIC, DO NOT DELETE
-""" for i in recipe[1]:
-    # print(parse_ingredient(i))
-    if any(MEAT in i for MEAT in MEATS):
-        print(i, 'MEAT')
-        tokens = parse_ingredient(i)
-        for j in range(len(tokens)):
-            if tokens[j] in MEATS:
-                tokens[j] = 'tofu'
-        print(tokens)
-for i in recipe[1]:
-    if any(protein in i for protein in VEG_PROTEINS):
-        print(i, 'PROTEIN')
-        tokens = parse_ingredient(i)
-        for j in range(len(tokens)):
-            if tokens[j] in VEG_PROTEINS:
-                tokens[j] = 'chicken'
-        print(tokens) """
+#p = load_page(URL)
+#recipe = extract_text(p)
+#print(recipe)
+#ingredients, steps = parse_recipe(recipe)
+#human_format(ingredients, steps)
 
 # TO DO
 # Doubling and halving quantities
 # Unhealthy to healthy
 # 
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    recipe_link = input("enter recipe url: ")
+    p = load_page(recipe_link)
+    recipe = extract_text(p)
+    ingredients, steps = parse_recipe(recipe)
+    print("transformation options are vegetarian, non-vegetarian, healthy, unhealthy, scale ingredient, italian, chinese, none")
+    transform = input("enter desired transform: ")
+    print("transforming... " + transform)
+    if (transform == "vegetarian"):
+        make_veg(ingredients, steps)
+    if (transform == "non-vegetarian"):
+        make_unveg(ingredients, steps)
+    if (transform == "healthy"):
+        make_healthy(ingredients)
+    if (transform == "unhealthy"):
+        make_unhealthy(ingredients)
+    if (transform == "scale ingredient"):
+        scale = input("enter scale factor ")
+        scale = float(scale)
+        scale_ingredient(ingredients, scale)
+    if (transform == "italian"):
+        make_italian(ingredients, steps)
+    if (transform == "chinese"):
+        make_chinese(ingredients, steps)
+    human_format(ingredients, steps)
